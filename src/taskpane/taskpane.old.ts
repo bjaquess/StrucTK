6f044dc46e9c0e7718d28ce3203bee70d7a0dd88
Evgeny Petukhov <evgeny.petukhov@gmail.com>
2021-02-13 03:36:30 +0300

    Task Pane as tree of components (support for forms, basic styles) with Umbrella.JS support

---
+++ src/taskpane/taskpane.old.ts
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+/* global console, document, Excel, Office */
+
+// The initialize function must be run each time a new page is loaded
+Office.initialize = () => {
+  document.getElementById("sideload-msg").style.display = "none";
+  document.getElementById("app-body").style.display = "flex";
+  document.getElementById("run").onclick = run;
+
+  document.getElementById('Template').onclick = onclick_Template;
+  document.getElementById('SeismicNonStructural').onclick = onclick_SeismicNonStructural;
+  document.getElementById('ConcRectFlexure').onclick = onclick_ConcRectFlexure;
+  document.getElementById('LoadComboTable').onclick = onclick_LoadCombinations;
+}
+
+const buttons = Array.from(document.querySelectorAll('.action-load-component'));
+buttons.map(async button => {
+  // eslint-disable-next-line no-undef
+  if (button instanceof HTMLElement) {
+    button.onclick = async () => {
+      const component = await import(`../components/${button.dataset.module}`);
+      await Excel.run(async context => {
+        await component.execute(context);
+      });
+    }
+  }
+})
+
+import { onclick_LoadCombinations } from "../TK Classes/Load Combinations/LoadCombinations_Controller";
+
+import { ConcFlexure_Controller } from "../TK Classes/Concrete Flexure/ConcFlexure_Controller";
+async function onclick_ConcRectFlexure() {
+  // *** Customize this section ******************************
+  let calcBlock = new ConcFlexure_Controller();
+  let onNewSheet: boolean = true;
+  let trialSheetName: string = 'ConcFlexure';
+  // *********************************************************
+  try {
+    await calcBlock.Initialize(onNewSheet, trialSheetName);
+    await Excel.run(async context => {
+      const range = context.workbook.getSelectedRange();
+      range.load("rowIndex");
+      range.load("columnIndex");
+      const ws = context.workbook.getSelectedRange().worksheet;
+      ws.load("name");
+      await context.sync();
+      const shapes = context.workbook.worksheets.getItem(ws.name).shapes;
+      //const charts = context.workbook.worksheets.getItem(ws.name).charts;
+      
+      calcBlock.Execute(range, range.rowIndex, range.columnIndex, shapes)
+    });
+  } catch (error) {console.error(error)}
+}
+
+import { Template_Controller } from "../TK Classes/Template/Template_Controller";
+async function onclick_Template() {
+  // *** Customize this section ******************************
+  let calcBlock = new Template_Controller();
+  let onNewSheet: boolean = true;
+  let trialSheetName: string = 'Template';
+  // *********************************************************
+  try {
+    await calcBlock.Initialize(onNewSheet, trialSheetName);
+    await Excel.run(async context => {
+      const range = context.workbook.getSelectedRange();
+      range.load("rowIndex");
+      range.load("columnIndex");
+      const ws = context.workbook.getSelectedRange().worksheet;
+      ws.load("name");
+      await context.sync();
+      const shapes = context.workbook.worksheets.getItem(ws.name).shapes;
+      const charts = context.workbook.worksheets.getItem(ws.name).charts;
+      
+      calcBlock.Execute(range, range.rowIndex, range.columnIndex, shapes, charts)
+    });
+  } catch (error) {console.error(error)}
+}
+
+
+import { SeismicNonStructuralComponents_Controller } from "../TK Classes/Seismic_Loads/Nonstructural/SeismicNonstructuralComponents_Controller";
+async function onclick_SeismicNonStructural() {
+  let calcBlock = new SeismicNonStructuralComponents_Controller();
+  try {
+    await Excel.run(async context => {
+      const range = context.workbook.getSelectedRange();
+      range.load("columnIndex");
+      range.load("rowIndex");
+      await context.sync();
+      let row : number = range.rowIndex;
+      let col : number = range.columnIndex;
+      calcBlock.ResultsToExcel(range, row, col);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+async function run() {
+  try {
+    await Excel.run(async context => {
+      /**
+       * Insert your Excel code here
+       */
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address
+      range.load("address");
+
+      // Update the fill color
+      range.format.fill.color = "yellow";
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -1,122 +1,34 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
+var u = require('umbrellajs');
+
 /* global console, document, Excel, Office */
 
-// The initialize function must be run each time a new page is loaded
 Office.initialize = () => {
-  document.getElementById("sideload-msg").style.display = "none";
-  document.getElementById("app-body").style.display = "flex";
-  document.getElementById("run").onclick = run;
-
-  document.getElementById('Template').onclick = onclick_Template;
-  document.getElementById('SeismicNonStructural').onclick = onclick_SeismicNonStructural;
-  document.getElementById('ConcRectFlexure').onclick = onclick_ConcRectFlexure;
-  document.getElementById('LoadComboTable').onclick = onclick_LoadCombinations;
+  // hide submenu, forms by default
+  u("section ul").addClass("hidden");
+  u("section ul li > button").addClass("hidden");
+  u("section ul li > form").addClass("hidden");
+  // Browse Structural Toolkit
+  // u("#run").on("click", showNav);
+  showNav();
+  initializeTree();
 }
 
-const buttons = Array.from(document.querySelectorAll('.action-load-component'));
-buttons.map(async button => {
-  // eslint-disable-next-line no-undef
-  if (button instanceof HTMLElement) {
-    button.onclick = async () => {
-      const component = await import(`../components/${button.dataset.module}`);
-      await Excel.run(async context => {
-        await component.execute(context);
-      });
+const showNav = () => {
+  u("#welcome").addClass("hidden");
+  u("nav").removeClass("hidden");
+}
+
+const initializeTree = () => {
+  u("h1").on("click", function(){
+    u(this).siblings("ul").toggleClass("hidden");
+  })
+  u("h2").on("click", function(){
+    u(this).siblings("button").toggleClass("hidden");
+  })
+  u("button").on("click", function(){
+    let toggleForm = u(this).first().id;
+    if (toggleForm) {
+      u("#"+toggleForm.replace("toggle-", "")).toggleClass("hidden");
     }
-  }
-})
-
-import { onclick_LoadCombinations } from "../TK Classes/Load Combinations/LoadCombinations_Controller";
-
-import { ConcFlexure_Controller } from "../TK Classes/Concrete Flexure/ConcFlexure_Controller";
-async function onclick_ConcRectFlexure() {
-  // *** Customize this section ******************************
-  let calcBlock = new ConcFlexure_Controller();
-  let onNewSheet: boolean = true;
-  let trialSheetName: string = 'ConcFlexure';
-  // *********************************************************
-  try {
-    await calcBlock.Initialize(onNewSheet, trialSheetName);
-    await Excel.run(async context => {
-      const range = context.workbook.getSelectedRange();
-      range.load("rowIndex");
-      range.load("columnIndex");
-      const ws = context.workbook.getSelectedRange().worksheet;
-      ws.load("name");
-      await context.sync();
-      const shapes = context.workbook.worksheets.getItem(ws.name).shapes;
-      //const charts = context.workbook.worksheets.getItem(ws.name).charts;
-      
-      calcBlock.Execute(range, range.rowIndex, range.columnIndex, shapes)
-    });
-  } catch (error) {console.error(error)}
-}
-
-import { Template_Controller } from "../TK Classes/Template/Template_Controller";
-async function onclick_Template() {
-  // *** Customize this section ******************************
-  let calcBlock = new Template_Controller();
-  let onNewSheet: boolean = true;
-  let trialSheetName: string = 'Template';
-  // *********************************************************
-  try {
-    await calcBlock.Initialize(onNewSheet, trialSheetName);
-    await Excel.run(async context => {
-      const range = context.workbook.getSelectedRange();
-      range.load("rowIndex");
-      range.load("columnIndex");
-      const ws = context.workbook.getSelectedRange().worksheet;
-      ws.load("name");
-      await context.sync();
-      const shapes = context.workbook.worksheets.getItem(ws.name).shapes;
-      const charts = context.workbook.worksheets.getItem(ws.name).charts;
-      
-      calcBlock.Execute(range, range.rowIndex, range.columnIndex, shapes, charts)
-    });
-  } catch (error) {console.error(error)}
-}
-
-
-import { SeismicNonStructuralComponents_Controller } from "../TK Classes/Seismic_Loads/Nonstructural/SeismicNonstructuralComponents_Controller";
-async function onclick_SeismicNonStructural() {
-  let calcBlock = new SeismicNonStructuralComponents_Controller();
-  try {
-    await Excel.run(async context => {
-      const range = context.workbook.getSelectedRange();
-      range.load("columnIndex");
-      range.load("rowIndex");
-      await context.sync();
-      let row : number = range.rowIndex;
-      let col : number = range.columnIndex;
-      calcBlock.ResultsToExcel(range, row, col);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-async function run() {
-  try {
-    await Excel.run(async context => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  })
 }
